0a2e7ab10bf957ed18ae1141d0f28335e351554c
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-11 19:23:26 +0530

    Update TouchEventsEg.js

--- TouchEventsEg.js
+++ TouchEventsEg.js
@@ -10,13 +10,50 @@ document.addEventListener('mousedown',mouseOnDown,false);
 document.addEventListener('mouseup',mouseOnUp,false);
 document.addEventListener('mousemove',mouseOnMove,false);
 
+document.addEventListener('touchstart',onDivTouchStart,false);
+document.addEventListener('touchmove',onDivTouchMove,false);
+document.addEventListener('touchend',onDivTouchEnd,false);
+
+
+function onDivTouchStart(e){
+	if (e.target.id === "innerDiv")
+	touchDiv = true;
+	console.log('downmouse : '+downMouse);
+}
+
+function onDivTouchMove(e){
+	e.preventDefault();
+	var touch = e.changedTouches[0];
+	var setLeft = touch.pageX + 'px';
+	var setTop = touch.pageY + 'px';
+	
+	if (touch.pageX > 400)
+		setLeft = "400px";
+	if (touch.pageX < 0)
+		setLeft = "0px";
+	if (touch.pageY > 400)
+		setTop = "400px";
+	if (touch.pageY < 0)
+		setTop = "0px";
+
+	
+	if (touchDiv)
+	{
+		innerDiv.style.left = setLeft;
+		innerDiv.style.top = setTop;
+	}
+}
+
+function onDivTouchEnd(e){
+	touchDiv = false;
+}
+
 function mouseOnDown(e){
 	console.log(e.target.id);                                                     // check if direct e.target obj can be compared
 	if (e.target.id === "innerDiv")
 	downMouse = true;
 	console.log('downmouse : '+downMouse);
 }
-
 
 function mouseOnMove(e){
 	
@@ -28,6 +65,8 @@ function mouseOnMove(e){
 	console.log('e.pageY : '+e.pageY);
 	var setLeft = e.pageX + 'px';
 	var setTop = e.pageY + 'px';
+	
+	//Made 400 to control the inner Div
 	if (e.pageX > 400)
 		setLeft = "400px";
 	if (e.pageX < 0)
