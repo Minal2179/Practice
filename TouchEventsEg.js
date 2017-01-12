73d5d912072cf317039bef81614f324076842483
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-11 20:05:18 -0800

    "touch events"

--- TouchEventsEg.js
+++ TouchEventsEg.js
@@ -1,11 +1,9 @@
 var downMouse = false;
-var clickX,clickY,innerDivX,innerDivY;
+var touc = false;
+var clickX,clickY,innerDivX,innerDivY,touchX,touchY;
+var fingerX = [],fingerY = [];
 var outerDiv = document.getElementById("outerDiv");
 var innerDiv = document.getElementById("innerDiv");
-// console.log(innerDiv.offsetLeft);
-// console.log(innerDiv.offsetTop);
-// console.log(outerDiv.offsetLeft);
-// console.log(outerDiv.offsetTop);
 
 document.addEventListener('mousedown',mouseOnDown,false);
 document.addEventListener('mouseup',mouseOnUp,false);
@@ -19,24 +17,40 @@ document.addEventListener('touchend',onDivTouchEnd,false);
 function onDivTouchStart(e){
 	if (e.target.id === "innerDiv")
 	touchDiv = true;
-	console.log('downmouse : '+downMouse);
+
+	var touchInnerDiv = e.touches[0];
+	touchX = parseInt(touchInnerDiv.clientX);
+	touchY = parseInt(touchInnerDiv.clientY);
+	innerDivX = parseInt(window.getComputedStyle(innerDiv,null).getPropertyValue("left"));
+	innerDivY = parseInt(window.getComputedStyle(innerDiv,null).getPropertyValue("top"));
+	
+	var numTouches = touchInnerDiv.length;
+	for (var i=0; i<e.touches[0].length; i++){ // loop through all touch points currently in contact with surface
+        //do something with each Touch object (point)
+		fingerX[i] = e.touches[i].clientX;
+		fingerY[i] = e.touches[i].clientY;
+		console.log('fingerX['+i+']'+fingerX[i]);
+		console.log('fingerY['+i+']'+fingerY[i]);
+	}
+
 }
 
 function onDivTouchMove(e){
 	e.preventDefault();
-	var touch = e.changedTouches[0];
-	var setLeft = touch.pageX + 'px';
-	var setTop = touch.pageY + 'px';
+	var touch = e.touches[0];
+	
+	var setLeft = parseInt(touch.pageX - (touchX - innerDivX))  + 'px';
+	var setTop = parseInt(touch.pageY - (touchY - innerDivY)) + 'px';
+
 	
 	if (touch.pageX > 400)
-		setLeft = "400px";
-	if (touch.pageX < 0)
-		setLeft = "0px";
-	if (touch.pageY > 400)
-		setTop = "400px";
-	if (touch.pageY < 0)
-		setTop = "0px";
-
+        setLeft = "400px";
+    if ((touch.pageX - (touchX - innerDivX)) < 0)
+        setLeft = "0px";
+    if (touch.pageY > 400)
+        setTop = "400px";
+    if ((touch.pageY - (touchY - innerDivY)) < 0)
+        setTop = "0px";
 	
 	if (touchDiv)
 	{
@@ -56,41 +70,27 @@ function mouseOnDown(e){
 	innerDivY = parseInt(window.getComputedStyle(innerDiv,null).getPropertyValue("top"));
 	clickX = parseInt(e.clientX);
 	clickY = parseInt(e.clientY);
-	console.log('clickX :' + clickX);
-	console.log('clickY :' + clickY);
-	console.log('innerDivX :' + innerDivX);
-	console.log('innerDivY :' + innerDivY);
-	console.log('clickX - innerDivX : '+(clickX - innerDivX));
-	console.log('clickY - innerDivY : '+(clickY - innerDivY));
-	console.log('downmouse : '+downMouse);
 }
 
 function mouseOnMove(e){
 	
 	e.preventDefault();
-	// console.log('mousemove');
-	// console.log('innerDiv.style.left : '+innerDiv.style.left);
-	// console.log('innerDiv.style.top : '+innerDiv.style.top);
-	// console.log('e.pageX : '+e.pageX);
-	// console.log('e.pageY : '+e.pageY);
-	var setLeft = parseInt(e.pageX + (clickX - innerDivX))  + 'px';
-	var setTop = parseInt(e.pageY + (clickY - innerDivY)) + 'px';
+	var setLeft = parseInt(e.pageX - (clickX - innerDivX))  + 'px';
+	var setTop = parseInt(e.pageY - (clickY - innerDivY)) + 'px';
 	
-	//Made 400 to control the inner Div
 	if (e.pageX > 400)
-		setLeft = "400px";
-	if (e.pageX < 0)
-		setLeft = "0px";
-	if (e.pageY > 400)
-		setTop = "400px";
-	if (e.pageY < 0)
-		setTop = "0px";
+        setLeft = "400px";
+    if ((e.pageX - (clickX - innerDivX)) < 0)
+        setLeft = "0px";
+    if (e.pageY > 400)
+        setTop = "400px";
+    if ((e.pageY - (clickY - innerDivY)) < 0)
+        setTop = "0px";
 		if (downMouse)
 		{
 			innerDiv.style.left = setLeft;
 			innerDiv.style.top = setTop;
-			// console.log('after set left : '+innerDiv.style.left);
-			// console.log('after set top : '+innerDiv.style.top);
+			
 		}
 
 }
