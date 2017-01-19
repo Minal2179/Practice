21ebede1b83384abd8ec368b63b5a63753f3ed8c
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-19 14:31:56 -0800

    "please use my xml for now. Somehow panel children are not working"

--- Day56.js
+++ Day56.js
@@ -1,53 +1,63 @@
 $(document).ready(function()
 {
 	$.get('Day56.xml', function(result){
-		//$(result).find('ui').each(function(){
-		//$(result).each(function(){
-			//var xmlElement = $(this).find('panel').text();
-			//var xmlElement = $(this).nodeName;
-			//alert(xmlElement);
-			//alert($(result)[0].childNodes[0].nodeName);
-			//alert($(result)[0].childNodes[0].childNodes[0].nodeValue);
-			alert($(result)[0].childNodes[0].children[0].children[0]);
-			//alert($(result)[0].childNodes[0].children.length);
-		//});
-		
+			$(result).find('ui').each(function(){
+				$(this).children().each(function(){
+					switch(this.tagName) {
+						case "panel":
+							var rectAttr = this.getAttribute('rect');
+							var uniqueID = this.getAttribute('uniquid');
+							createElementUI(uniqueID,rectAttr,'','',this.tagName,'body');
+							$(this).children().each(function(){
+								var rectAttr = this.getAttribute('rect');
+								var uniqueID = this.getAttribute('uniquid');
+								alert("uniqueID, tagName : "+uniqueID+", "+this.tagName);
+								createElementUI(uniqueID,rectAttr,'','',this.tagName,'panel');
+							});
+							break;
+						case "button":
+							var rectAttr = this.getAttribute('rect');
+							var uniqueID = this.getAttribute('uniquid');
+							var textVal = this.childNodes[0].nodeValue;
+							createElementUI(uniqueID,rectAttr,textVal,'',this.tagName,'body');
+							   break;
+						case "image":
+							var rectAttr = this.getAttribute('rect');
+							var uniqueID = this.getAttribute('uniquid');
+							var imgSrc = this.getAttribute('imgSrc');
+							var textVal = this.childNodes[0].nodeValue;
+							createElementUI(uniqueID,rectAttr,textVal,imgSrc,this.tagName,'body');
+							break;
+						default:
+							break;
+					}
+				});
+			});	
 	});
-})
+});
 
-/*
-
-xml.find('ui').each(function(){
-    $(this).children().each(function(){
-        var tagName=this.tagName; // This will give Panel, button, image and Panel
-        $(this).children().each(function(){
-			var innerTagName = this.tagName; 
-		})
-		//var val=$(this).text();
-        //if(val==1){
-        //    $('input:checkbox[name='+tagName+']').attr('checked',true);
-        //}
-        //else if(val==0){
-        //    $('input:checkbox[name='+tagName+']').removeAttr('checked');
-        //}
-    })
-}); 
-
-*********************************
-
-http://anasthecoder.blogspot.in/2012/02/looping-through-xml-with-jquery.html 
-
-xml.find('result').find('permissionDetails').each(function(){
-    $(this).children().each(function(){
-        var tagName=this.tagName;
-        var val=$(this).text();
-        if(val==1){
-            $('input:checkbox[name='+tagName+']').attr('checked',true);
-        }
-        else if(val==0){
-            $('input:checkbox[name='+tagName+']').removeAttr('checked');
-        }
-    })
-}); 
-
-*/
+function createElementUI(uniqueID,rectAttr,textVal,imgSrc,elementName,parentElement){
+		switch(elementName){
+			case "panel":
+				createEle = "div";
+				break;
+			case "button":
+				createEle = "button";
+				break;
+			case "label":
+				createEle = "label";
+				break;
+			case "image":
+				createEle = "img";
+				break;
+		}
+		var coordinates = rectAttr.split(',');
+		var uiElement = document.createElement(createEle);
+		uiElement.setAttribute("id", uniqueID);
+		if(imgSrc != ''){
+			uiElement.setAttribute("src",imgSrc);
+		}
+		$(parentElement).append(uiElement);
+		$('#'+uniqueID).css({'left':coordinates[0]+'px','top':coordinates[1]+'px','width':coordinates[2]+'px',"height":coordinates[3]});
+		$('#'+uniqueID).text(textVal);
+}
