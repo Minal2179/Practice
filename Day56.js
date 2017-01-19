2a15719b84986cba7c755372e5947c601c86142e
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-19 18:50:31 +0530

    Update Day56.js

--- Day56.js
+++ Day56.js
@@ -14,3 +14,40 @@ $(document).ready(function()
 		
 	});
 })
+
+/*
+
+xml.find('ui').each(function(){
+    $(this).children().each(function(){
+        var tagName=this.tagName; // This will give Panel, button, image and Panel
+        $(this).children().each(function(){
+			var innerTagName = this.tagName; 
+		})
+		//var val=$(this).text();
+        //if(val==1){
+        //    $('input:checkbox[name='+tagName+']').attr('checked',true);
+        //}
+        //else if(val==0){
+        //    $('input:checkbox[name='+tagName+']').removeAttr('checked');
+        //}
+    })
+}); 
+
+*********************************
+
+http://anasthecoder.blogspot.in/2012/02/looping-through-xml-with-jquery.html 
+
+xml.find('result').find('permissionDetails').each(function(){
+    $(this).children().each(function(){
+        var tagName=this.tagName;
+        var val=$(this).text();
+        if(val==1){
+            $('input:checkbox[name='+tagName+']').attr('checked',true);
+        }
+        else if(val==0){
+            $('input:checkbox[name='+tagName+']').removeAttr('checked');
+        }
+    })
+}); 
+
+*/
