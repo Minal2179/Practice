0b777920193371ab0e12dfd620e908146acf5fc3
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-18 18:31:03 -0800

    "xml WIP"

--- Day56.js
+++ Day56.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     $("button").click(function(){
 		alert('hi2');
-        $.get('Day56.xml', function(result){
-			alert("hi3");
-           $("#xmlContent").text(result);
-        });
+        $.ajax({url:'Day56.xml', success:function(result){
+				$(result).find('ui').each(function(){
+					//https://code.tutsplus.com/tutorials/quick-tip-use-jquery-to-retrieve-data-from-an-xml-file--net-390
+					// https://www.google.co.in/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=jquery+read+xml+examples
+					//https://www.xml.com/pub/a/2007/10/10/jquery-and-xml.html
+					
+				});
+			});
+        }});
     });
 });
