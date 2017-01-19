32a707ec10eb15fafe2cd420949a4649e6b9f6d0
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-19 18:49:19 +0530

    Update Day56.js

--- Day56.js
+++ Day56.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-    $("button").click(function(){
-		alert('hi2');
-        $.ajax({url:'Day56.xml', success:function(result){
-				$(result).find('ui').each(function(){
-					//https://code.tutsplus.com/tutorials/quick-tip-use-jquery-to-retrieve-data-from-an-xml-file--net-390
-					// https://www.google.co.in/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=jquery+read+xml+examples
-					//https://www.xml.com/pub/a/2007/10/10/jquery-and-xml.html
-					
-				});
-			});
-        }});
-    });
-});
+$(document).ready(function()
+{
+	$.get('Day56.xml', function(result){
+		//$(result).find('ui').each(function(){
+		//$(result).each(function(){
+			//var xmlElement = $(this).find('panel').text();
+			//var xmlElement = $(this).nodeName;
+			//alert(xmlElement);
+			//alert($(result)[0].childNodes[0].nodeName);
+			//alert($(result)[0].childNodes[0].childNodes[0].nodeValue);
+			alert($(result)[0].childNodes[0].children[0].children[0]);
+			//alert($(result)[0].childNodes[0].children.length);
+		//});
+		
+	});
+})
