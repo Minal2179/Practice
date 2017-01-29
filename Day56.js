13d004fd156d2df06167c6386c5119b1846e9925
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-01-29 04:49:44 -0800

    "test panel inside panel"

--- Day56.js
+++ Day56.js
@@ -9,7 +9,7 @@ $(document).ready(function()
 			if (mainElements[i].tagName === 'panel')
 			{
 				//console.log("have to draw panel");
-				drawDiv(mainElements[i]);
+				drawDiv(mainElements[i],"");
 			}
 			else
 			{
@@ -19,8 +19,8 @@ $(document).ready(function()
 		
 	});
 });
-	function drawDiv(elementObject){
-		createElement(elementObject,"");
+	function drawDiv(elementObject,parentElement){
+		createElement(elementObject,parentElement);
 		var divElements = elementObject.children;
 		//console.log("elementObject.tagname : "+elementObject.tagName);
 		//console.log("elementObject.children.length : "+elementObject.children.length);
@@ -29,7 +29,7 @@ $(document).ready(function()
 			if(elementObject.children[j].tagName == "panel")
 			{
 				//console.log("no child panel");
-				drawDiv(elementObject.children[j]);
+				drawDiv(elementObject.children[j],divElements[j].parentElement.attributes["uniquid"].value);
 			}
 			else
 			{
@@ -40,6 +40,7 @@ $(document).ready(function()
 		}
 	}
 	function createElement(elementObject,parentElement){
+		imgSrc = "";
 		//console.log(elementObject.tagName);
 		switch(elementObject.tagName){
 			case "button":
@@ -47,17 +48,31 @@ $(document).ready(function()
 				break;
 			case "image":
 				ele = "img";
+				imgSrc = elementObject.attributes["imgSrc"].value;
 				break;
 			case "panel":
 				ele = "div";
+				break;
+			case "label":
+				ele = "label"
 				break;
 		}
 			var uniqueID = elementObject.attributes["uniquid"].value;
 			var coordinates = elementObject.attributes["rect"].value.split(",");
 			var uiElement = document.createElement(ele);
 			uiElement.setAttribute("id",uniqueID);
-			$(uiElement).css({'left':coordinates[0]+'px','top':coordinates[1]+'px','width':coordinates[2]+'px',"height":coordinates[3]});
+			if (imgSrc == "")
+			{
+			}
+			else 
+				uiElement.setAttribute("src",imgSrc);
+			$(uiElement).css({left: coordinates[0]+'px',top: coordinates[1]+'px',width: coordinates[2]+'px',height: coordinates[3]});
+			if(elementObject.tagName == "panel"){
+			}
+			else
+			{
 			$(uiElement).text(elementObject.textContent);
+			}
 			if(parentElement === "")
 				$("body").append(uiElement);
 			else
