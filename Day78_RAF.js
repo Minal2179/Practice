e6691295b03ca8f176eb54e1231ab0ad56d8b8ff
AnushreeDesai35 <anushreerdesai@gmail.com>
2017-02-05 20:11:48 -0800

    "RAF"

--- Day78_RAF.js
+++ Day78_RAF.js
@@ -1,23 +1,7 @@
 
+var i=0,j=0;
 function bouncingBall() {
-	var drop = 1;
-	$(".ball").css({left:'550px'});
-	var i=0;
-	for (i;i<3;i++){
-		if (drop == 1)
-		{
-			//$(".ball").css({top:'550px'});
-			$(".ball").removeClass('rise');
-			$(".ball").addClass('drop');
-			drop = 0;
-		}
-		else
-		{
-			$(".ball").removeClass('drop');
-			$(".ball").addClass('rise');
-			drop = 1;
-		}
-	}
+			$(".ball").offset({left	:i=i+3,top:j=j+2});
 	requestAnimationFrame(bouncingBall);
 }
 
